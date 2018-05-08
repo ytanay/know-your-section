c10c505aff517526fb22b1ef61356b7fd005679d
Yotam Tanay <yotam.tanay@forter.com>
2018-05-08 20:48:34 +0300

    Entry point

---
+++ chat.py
@@ -0,0 +1,21 @@
+#!/bin/env python
+from app import create_app, socketio
+
+app = create_app(debug=True)
+
+
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=5000)
